6d67976a6f46b7cc60944960998ee38be5169f51
Patrick-Njiru <patrick.njiru@student.moringaschool.com>
2022-08-22 11:45:39 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+
+let totalBatteries =
+ 0
+function reducer(batteryBatches) {
+    for (const number of batteryBatches) {
+        totalBatteries += number
+    }
+    return totalBatteries
+}
+
+console.log(reducer(batteryBatches));
