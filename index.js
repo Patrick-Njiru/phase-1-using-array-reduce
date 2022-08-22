d5cc4daa9e88d938ea545d84a251e375d1ffaa1f
Patrick-Njiru <patrick.njiru@student.moringaschool.com>
2022-08-22 14:01:08 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -2,13 +2,24 @@ const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
 
-let totalBatteries =
- 0
-function reducer(batteryBatches) {
-    for (const number of batteryBatches) {
-        totalBatteries += number
-    }
+// let totalBatteries =
+//  0
+// function reducer(batteryBatches) {
+//     for (const number of batteryBatches) {
+//         totalBatteries += number
+//     }
+//     return totalBatteries
+// }
+
+// console.log(reducer(batteryBatches));
+
+
+
+let totalBatteries = 0
+
+ const reducer = batteryBatches => {
+    totalBatteries = batteryBatches.reduce (((accumulator, currentValue) => accumulator + currentValue), 0)
     return totalBatteries
-}
+ }
 
 console.log(reducer(batteryBatches));
